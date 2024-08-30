047450abb4dfb0c775f3e54a11e1625ced53d6be
ciringa <thierrird957468@gmail.com>
2024-08-30 11:31:50 -0300

    Setup of jwt validation

--- src/http/controlers/Post/createPost.ts
+++ src/http/controlers/Post/createPost.ts
@@ -1,15 +1,15 @@
-import { FastifyReply, FastifyRequest } from "fastify";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 import {prisma} from "../../../lib/prisma"
 
 export async function CreateAPostControler(req:FastifyRequest, res:FastifyReply) {
-    const {Content,Slug,ownerId,Title} = z.object({
+    
+    const {Content,Slug,Title} = z.object({
         Content:z.string(),
         Title:z.string().optional(),
         Slug:z.string(),
-        ownerId:z.string().uuid()
     }).parse(req.body)
-
+    const ownerId = req.user.sub
     try{
         const response = await prisma.posts.create({
             data:{
--- src/http/router.ts
+++ src/http/router.ts
@@ -4,12 +4,15 @@ import { ReturnPostListControler } from "./controlers/Post/PostLists";
 import { CreateAPostControler } from "./controlers/Post/createPost";
 import { FindUserbyIdControler } from "./controlers/User/finUserById";
 import { LoginControler } from "./controlers/User/loginControler";
+import { VerifyJWT } from "./midlewares/VerifyJwt";
 
 
 export async function router(app:FastifyInstance) {
     app.post("/user/create",CreateUserControler)
     app.get("/posts/all/:page",ReturnPostListControler)
-    app.post("/posts/create",CreateAPostControler)
+    app.post("/posts/create",{
+        preHandler:[VerifyJWT]
+    },CreateAPostControler)
     app.get("/users/:UId",FindUserbyIdControler)
     app.patch("/user/login",LoginControler)
 }
